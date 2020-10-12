5ed758a3e9b2b0ac79282ae0de7b5ec942a4b00e
Ubuntu <ubuntu@ip-172-31-25-121.us-east-2.compute.internal>
2020-10-12 16:36:57 +0000

    base /upload-file endopoint

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-const http = require('http');
+const express = require("express");
+const server = express();
 
-const hostname = '127.0.0.1';
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('AeroCivil FileLoader');
-});
 
 server.listen(PORT, () => {
-  console.log(`El servidor se está ejecutando en http://${hostname}:${PORT}/`);
+  console.log(`El servidor se está ejecutando en localhost:${PORT}/`);
 });
 
 server.get('/upload-file', function (req, res) {
