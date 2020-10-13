62fa474dc90cf806abf44ab5cf5de58a0626d144
Ubuntu <ubuntu@ip-172-31-25-121.us-east-2.compute.internal>
2020-10-13 23:58:21 +0000

    serve google drive API example

--- index.js
+++ index.js
@@ -1,43 +1,19 @@
 const axios = require('axios');
 const fs = require('fs');
-var btoa = require('btoa');
 const express = require("express");
 const server = express();
 
 const PORT = process.env.PORT || 5000;
 
-var authStr = btoa('ADMISIONES:S0porte*2020');
-
-const fileName = 'testfile.txt';
-
-var strAjaxUrl = 'http://www.aerocivil.gov.co'
-  + "/_api/web/lists/getByTitle('ADMISIONES')"
-  + "/RootFolder/files/add(overwrite=true,url='"+ fileName +"')";
-  
-var newFile = fs.createReadStream('testfile.txt');
-  
   
 
 server.listen(PORT, () => {
   console.log(`El servidor se está ejecutando en localhost:${PORT}/`);
 });
 
-server.get('/upload-file', function (req, res) {
-  axios
-    .post(strAjaxUrl, newFile,{
-      headers: {
-        'Content-Type': 'multipart/form-data',
-        'Authorization': 'Basic ' + authStr
-      }
-    })
-    .then(res => {
-      console.log(res);
-      res.send('Success on file upload.');
-    })
-    .catch(error => {
-      console.error(error);
-      res.send('Error on file upload.');
-    })
+server.get('/drive-api-test', function (req, res) {
+  res.writeHead(200, { 'content-type': 'text/html' });
+  fs.createReadStream('drive_api_test.html').pipe(res);
 });
 
 
